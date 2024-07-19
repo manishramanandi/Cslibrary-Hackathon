45d0ae5a1e37f7151cb25e6149a5e7e28af64fb3
manishramanandi <bairagimanish222@gmail.com>
2024-07-19 07:04:42 +0530

    Designed Login page

--- Cslibrary-svelte-vite/src/App.svelte
+++ Cslibrary-svelte-vite/src/App.svelte
@@ -1,29 +1,28 @@
 <!-- App.svelte -->
 <script>
-	import { Router, Link, Route } from "svelte-navigator";
-	import Home from "./Component/Home.svelte";
-  import Books from "./Component/Books.svelte";
-  import About from "./Component/About.svelte";
-  import Contact from "./Component/Contact.svelte";
-  import Signup from "./Component/Signup.svelte";
-  import Login from "./Component/Login.svelte";
+    import { Router, Link, Route } from "svelte-navigator";
+    import Home from "./Component/Home.svelte";
+    import Books from "./Component/Books.svelte";
+    import About from "./Component/About.svelte";
+    import Contact from "./Component/Contact.svelte";
+    import Login from "./Component/Login.svelte";
+    import Signup from "./Component/Signup.svelte";
 </script>
-<Router>
-  <nav>
-  </nav>
-  <div>
-    <Route path="/" component={Home} />
-    <Route path="about" component={About} />
-    <Route path="books" component={Books} />
-    <Route path="contact" component={Contact} />
-    <Route Path="signup" component={Signup} />
-    <Route Path="login" component={Login} />
 
-  </div>
+<Router>
+    <nav></nav>
+    <div>
+        <Route path="/" component={Home} />
+        <Route path="about" component={About} />
+        <Route path="books" component={Books} />
+        <Route path="contact" component={Contact} />
+        <Route Path="signup" component={Signup} />
+        <Route Path="login" component={Login} />
+    </div>
 </Router>
 
 <style>
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
 </style>
